a48d937388843c21d76de5844e2ba1d6178afdbb
Homura-san <erickbispo@ufba.br>
2025-10-10 17:52:44 -0300

    useEffect #01

--- src/views/examples/UseEffect.jsx
+++ src/views/examples/UseEffect.jsx
@@ -1,13 +1,46 @@
 import React from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
+
+function calcFatorial(num) {
+    const n = parseInt(num)
+    if (n < 0) return -1
+    if (n === 0) return 1
+    return calcFatorial(n - 1) * n
+}
 
 const UseEffect = (props) => {
+    const [number, setNumber] = React.useState(1)
+    const [fatorial, setFatorial] = React.useState(1)
+
+    React.useEffect(function () {
+        setFatorial(calcFatorial(number))
+    }, [number])
+
+    React.useEffect(function () {
+        if (fatorial > 1000) {
+            document.title = "Eita!!!"
+        }
+    }, [fatorial])
+
     return (
         <div className="UseEffect">
             <PageTitle
                 title="Hook UseEffect"
                 subtitle="Permite executar efeitos colaterais em componentes funcionais!"
             />
+
+            <SectionTitle title="Exercício #01" />
+            <div className="center">
+                <span className="text"> Fatorial: </span>
+                <span className="text red"> {fatorial === -1 ? 'Não existe' : fatorial} </span>
+                <input type="number" className='input' value={number}
+                    onChange={e => setNumber(e.target.value)} />
+            </div>
+
+            <SectionTitle title="Exercício #02" />
+            <div className="center"></div>
+
         </div>
     )
 }
