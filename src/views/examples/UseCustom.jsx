a9e571cf57a6c4e889b891017d3d317a4bf1c431
Homura-san <erickbispo@ufba.br>
2025-10-27 18:56:24 -0300

    Custom Hooks #01

--- src/views/examples/UseCustom.jsx
+++ src/views/examples/UseCustom.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { use } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
+import { useCounter } from '../../hooks/useCounter'
 
 const UseRef = (props) => {
+
+     const [count, inc, dec] = useCounter(17)
+     const url = 'http://files.cod3r.com.br/curso-react/estados.json'
+     const state = useFetch(url)
+
     return (
         <div className="UseCustom">
             <PageTitle
                 title="Seu Hook"
                 subtitle="Vamos aprender como criar o nosso próprio Hook!"
             />
+
+            <SectionTitle title="Exercício #01" />
+            <div className="center">
+                <span className="text">{count}</span>
+                <div>
+                    <button className="btn" onClick={() => inc()}>+1</button>
+                    <button className="btn" onClick={() => dec()}>-1</button>
+                </div>
+            </div>
+
+            <SectionTitle title="Exercício #02" />
+            <div className="center"></div>
         </div>
     )
 }
